e7e5d9b7f345f481caef703a4705ab736ab29a56
Yuntian <da03@users.noreply.github.com>
2018-07-03 21:18:57 -0400

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -14,6 +14,7 @@ let package = Package(
     targets: [
         .target(
             name: "CZeroMQ",
-            dependencies: []),
+            dependencies: [],
+            path: "."),
     ]
 )
